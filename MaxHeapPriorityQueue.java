ad451d90564f766f3294b3364cd7e18e11846e96
mrina <golemrinal987@gmail.com>
2023-08-11 22:32:13 +0545

    changes

---
+++ MaxHeapPriorityQueue.java
@@ -0,0 +1,19 @@
+// import java.util.PriorityQueue;
+// import java.util.Comparator;
+
+// public class MaxHeapPriorityQueue {
+//     public static void main(String[] args) {
+//         // Creating a maximum heap (priority queue)
+//         PriorityQueue<Integer> maxHeap = new PriorityQueue<>(Comparator.reverseOrder());
+
+//         // Adding elements
+//         maxHeap.add(5);
+//         maxHeap.add(8);
+//         maxHeap.add(2);
+//         maxHeap.add(10);
+
+//         // Removing elements (removes maximum element)
+//         System.out.println(maxHeap.poll()); // Output: 10
+//         System.out.println(maxHeap.poll()); // Output: 8
+//     }
+// }
